6f8750c0ffc99fed44deecd7a240af97acf530c2
Lincoln Touw <149920595+lincolntouw@users.noreply.github.com>
2025-06-24 13:59:08 -0400

    Implemented Region.center

--- Regions.lua
+++ Regions.lua
@@ -1,6 +1,7 @@
 export type Region = {
-	Min: Vector3,
-	Max: Vector3, 		
+	Min: Vector3, -- minimum
+	Max: Vector3, -- maximum		
+	Point: Vector3, -- center (avg) 																			
 	new: (Min: Vector3, Max: Vector3) -> Region,		 
 	within: (self: Region, Target: Vector3) -> boolean,
 };		
@@ -11,7 +12,8 @@ Region.__index = Region;
 Region.new = function(Min: Vector3, Max: Vector3): Region	
 	return setmetatable({
 		Min = Min,
-		Max = Max
+		Max = Max,
+		Point = (Min + Max) / 2		
 	}, Region);		 
 end;	 
 -- Checks if the target vector is within the range. 	
@@ -21,4 +23,15 @@ function Region.within(self: Region, Target: Vector3): boolean
 		IsBetween(Target.Y, self.Min.Y, self.Max.Y) and
 		IsBetween(Target.Z, self.Min.Z, self.Max.Z);	
 end; 		
+-- Returns the center point among a list of regions.	 
+function Region.center(r1: Region, ...: Region?): Vector3	
+	local list = table.pack(...); 	
+	list.n = nil;
+	table.insert(list, 1, r1);	 
+	local total = Vector3.zero;	
+	for _, v: Region? in list do
+		total += v.Point;
+	end;		 
+	return total / #list;	 			
+end;	
 return Region;	 
